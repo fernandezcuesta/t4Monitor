f41df947a455be6149227c70923c37e39d19fe8e
Jesús Fernández <fernandez.cuesta@gmail.com>
2015-12-15 16:28:05 +0100

    Move windows installation to batch file

--- install_windows.bat
+++ install_windows.bat
@@ -1,1 +1,4 @@
-pip install --no-index --find-links=local/ -r requirements-windows.txt
+conda create -n t4Monitor --file requirements-conda.txt -y
+activate t4Monitor
+pip install -r requirements-common.txt
+pip install --no-index --find-links=local pycairo
