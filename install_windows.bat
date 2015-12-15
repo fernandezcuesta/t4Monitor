3b473d400e9e22bf8ec521a6f05261e526982a6e
Jesús Fernández <fernandez.cuesta@gmail.com>
2015-12-15 16:37:12 +0100

    Update install_win

--- install_windows.bat
+++ install_windows.bat
@@ -1,4 +1,4 @@
-conda create -n t4Monitor --file requirements-conda.txt -y
-activate t4Monitor
 pip install -r requirements-common.txt
 pip install --no-index --find-links=local pycairo
+python setup.py build
+python setup.py install
